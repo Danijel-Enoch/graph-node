21a711d654fc906a621d527e8d17c4b3bf2bf7fb
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2023-03-03 15:01:17 -0500

    feat(graphql): return true for field names that end with paginated
    when we create a connection objects it is
    ```graphql
    type CarConnection {
     # other fields
    }
    
    type Query {
     carsPaginated: CarConnection!
    }
    ```
    
    a Connection field's name will always end with `Paginated` so given Object type or field name we resolve to connection types.

--- graphql/src/schema/connection.rs
+++ graphql/src/schema/connection.rs
@@ -1,9 +1,12 @@
 pub fn is_connection_type(field_or_type_name: &str) -> bool {
-    field_or_type_name.ends_with("Connection")
+    // the field return type will have *Connection
+    // or the field name will be *Paginated
+    field_or_type_name.ends_with("Connection") || field_or_type_name.ends_with("Paginated")
 }
 
 #[test]
 fn test_is_valid_connection_type() {
     assert!(is_connection_type("FooConnection"));
+    assert!(is_connection_type("barPaginated"));
     assert!(!is_connection_type("Foo"));
 }
