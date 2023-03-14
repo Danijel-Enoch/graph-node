dd4e546b7bc47ec6660a7c622b036834f74a56c9
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2023-03-14 16:41:33 -0400

    Revert "feat(graphql): return true for field names that end with paginated"
    This reverts commit 52751cb9dfef9c2125b124ff340e46f5ab953991.

--- graphql/src/schema/connection.rs
+++ graphql/src/schema/connection.rs
@@ -1,12 +1,9 @@
 pub fn is_connection_type(field_or_type_name: &str) -> bool {
-    // the field return type will have *Connection
-    // or the field name will be *Paginated
-    field_or_type_name.ends_with("Connection") || field_or_type_name.ends_with("Paginated")
+    field_or_type_name.ends_with("Connection")
 }
 
 #[test]
 fn test_is_valid_connection_type() {
     assert!(is_connection_type("FooConnection"));
-    assert!(is_connection_type("barPaginated"));
     assert!(!is_connection_type("Foo"));
 }
